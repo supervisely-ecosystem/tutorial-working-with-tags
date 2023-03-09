b26fbaf7032fb72183b9527e45fd7738930a842d
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2023-03-09 15:30:36 +0000

    if sly.is_development() added

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ import os
 from dotenv import load_dotenv
 import supervisely as sly
 
-load_dotenv("local.env")
-load_dotenv(os.path.expanduser("~/supervisely.env"))
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 api = sly.Api()
 
 ####################
